53d727c951c42815e4b59110ad9700e72c4ef1c1
SAK74 <sergijks@gmail.com>
2024-05-11 10:48:33 +0200

    feat: add cross cross btn to detail view

--- app/(home)/@detail/detail/[id]/page.tsx
+++ app/(home)/@detail/detail/[id]/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { getCharacterById } from "@/services/getCharacterById";
 import Link from "next/link";
 import { Search } from "../../../layout";
+import { GiSplitCross as CrossIcon } from "react-icons/gi";
 
 export default async function Page({
   params: { id },
@@ -12,17 +13,22 @@ export default async function Page({
   searchParams: Search["searchParams"];
 }) {
   const character = await getCharacterById(id);
-  // console.log(Page.name, { id, searchParams });
   if (typeof character === "string") {
     return character;
   }
+  const href = "/?" + new URLSearchParams(searchParams).toString();
   return (
-    <div className="px-6 flex flex-col-reverse md:flex-col gap-4 ">
+    <div className="px-6 flex flex-col-reverse md:flex-col gap-4 relative">
+      <Link
+        href={href}
+        className="absolute top-3 left-8 p-1 rounded-sm hover:border active:scale-90"
+      >
+        <CrossIcon size={26} className="fill-destructive/80" />
+      </Link>
+
       <DetailCharacterCard character={character} />
       <Button size="lg" className="self-center">
-        <Link href={"/?" + new URLSearchParams(searchParams).toString()}>
-          Close details
-        </Link>
+        <Link href={href}>Close details</Link>
       </Button>
     </div>
   );
